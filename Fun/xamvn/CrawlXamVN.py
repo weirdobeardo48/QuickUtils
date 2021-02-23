b9eb1f11a85ab1c4c7784e6ae458da3c66620218
weirdobeardo48 <truongnx@easyshoppingfeed.com>
2021-02-23 15:44:05 +0700

    20210223 | Change selenium to requests and beautifulsoup

---
+++ Fun/xamvn/CrawlXamVN.py
@@ -0,0 +1,139 @@
+#!/usr/bin/python3
+from Fun.xamvn.XamVNUtils import XamVN
+from selenium import webdriver
+import requests
+import os
+import logging
+import logging.config
+import configparser
+import argparse
+import json
+import traceback
+
+# Take argument
+parser = argparse.ArgumentParser()
+
+parser.add_argument('--url', required=True, help="URL to xamvn thread")
+parser.add_argument(
+    '--fromPage', help="The page you want crawler to start from")
+parser.add_argument('--toPage', required=True,
+                    help="The page you want crawler to finish at")
+# At the moment, I haven't done the login part yet, pull request please?
+parser.add_argument('--username',
+                    help="Your user name, not necessary, but required you have to login first, to have the cookies")
+parser.add_argument('--password',
+                    help="Your password, not necessary, but required you have to login first, to have the cookies")
+parser = parser.parse_args()
+
+URL = parser.url
+user_name = parser.username
+password = parser.password
+from_page = parser.fromPage
+to_page = int(parser.toPage)
+
+# We are gonna read config from config files by using this function
+config = None
+
+
+def init_config():
+    # print('Loading config at startup!')
+    global config
+    config = configparser.ConfigParser()
+    config.read('config/configs.ini')
+
+
+# End init config, call it in what ever you want to read, or re-read!
+
+
+# This help us easier in defining log!
+
+log = logging.getLogger(__name__)
+
+
+def init_log():
+    global config
+    # Just a workaround, but whatever, this just a cheap script
+    if os.path.join(config['LOGGER']['log_file_path']):
+        if not os.path.exists(os.path.join(config['LOGGER']['log_file_path'])):
+            os.makedirs(os.path.join(config['LOGGER']['log_file_path']))
+    logging_config = {
+        'version': 1,
+        'disable_existing_loggers': False,
+        'formatters': {
+            'standard': {
+                'format': '%(asctime)s [%(levelname)s] %(name)s: %(lineno)d: %(message)s'
+            },
+        },
+        'handlers': {
+            'default_handler': {
+                'class': 'logging.handlers.TimedRotatingFileHandler',
+                'level': config['LOGGER']['file_log_level'],
+                'formatter': 'standard',
+                'filename': os.path.join(config['LOGGER']['log_file_path'], 'application.log'),
+                'encoding': 'utf8',
+                'backupCount': 10,
+                'when': 'd',
+                'interval': 1,
+            },
+            'stdout_handler': {
+                'class': 'logging.StreamHandler',
+                'level': config['LOGGER']['std_out_log_level'],
+                'formatter': 'standard'
+            }
+        },
+        'loggers': {
+            '': {
+                'handlers': ['default_handler', 'stdout_handler'],
+                'level': config['LOGGER']['default_log_level'],
+                'propagate': False
+            }
+        }
+    }
+
+    logging.config.dictConfig(logging_config)
+
+
+# End defining log
+
+def read_json_file_to_dict(file_path) -> dict:
+    with open(file=file_path, mode="r") as f:
+        text = "".join(f.readlines())
+        return json.loads(text)
+
+
+def get_headers() -> dict:
+    return read_json_file_to_dict('./Fun/xamvn/files/headers.json')
+
+
+def get_cookies() -> dict:
+    return read_json_file_to_dict('./Fun/xamvn/files/cookies.json')
+
+
+if __name__ == '__main__':
+    # Read config
+    init_config()
+    # Init log
+    init_log()
+
+    log.info("Let's get started")
+    log.debug("Arguments:")
+    log.debug(parser)
+
+    headers = get_headers()
+    cookies = get_cookies()
+
+    if headers is not None:
+        try:
+            xamvn = XamVN(headers=headers, cookies=cookies)
+
+            xamvn.apply_params(config=config, from_page=from_page,
+                               to_page=to_page, URL=URL)
+
+            # Do the job
+            xamvn.crawl()
+        except Exception as e:
+            traceback.print_exc()
+            log.exception(e)
+
+    else:
+        log.error("Specify your headers in a file call headers.json")
---
+++ Fun/xamvn/XamVNUtils.py
@@ -0,0 +1,97 @@
+import logging
+import os
+import requests
+from bs4 import BeautifulSoup
+import time
+
+
+class XamVN:
+
+    __current_page = 1
+    __requests_headers: dict = None
+    __cookies: dict = None
+    f = requests.Session()
+    __log = logging.getLogger(__name__)
+
+    def __init__(self, headers: dict, cookies: dict) -> None:
+        self.__requests_headers = headers
+        self.__cookies = cookies
+        self.__log.info("XamVNUtils is initialized!!")
+        pass
+
+    def apply_params(self, **kwargs) -> None:
+        if 'URL' in kwargs:
+            self.__URL = kwargs['URL']
+        if 'from_page' in kwargs:
+            self.__from_page = kwargs['from_page']
+        if 'to_page' in kwargs:
+            self.__to_page = kwargs['to_page']
+        if 'config' in kwargs:
+            self.__config = kwargs['config']
+
+    def get_element_url_and_download(self, elements) -> None:
+        count = 0
+        all_link = set()
+        for element in elements:
+            # link = element.get_attribute('src')
+            link = element['src']
+            if link is not None and not str(link).startswith("https://xamvn.us"):
+                link = "https://xamvn.us" + link
+            # Getting images that are uploaded by user, or embed by users, not avatar, logo, blah
+            if 'attachments' in link or 'proxy.php' in link or 'video' in link:
+                all_link.add(link)
+
+        for link in all_link:
+            self.__log.info("Getting URL: %s" % link)
+
+            self.__log.info("Sleeping for %s second" %
+                            str(self.__config['CHROME-DRIVER']['interval-between-crawl']))
+            time.sleep(int(self.__config['CHROME-DRIVER']
+                           ['interval-between-crawl']))
+            count += 1
+            req = self.f.get(
+                link, headers=self.__requests_headers, cookies=self.__cookies)
+            self.__log.info("Status code: " + str(req.status_code))
+            if req.status_code == 200:
+                self.__log.info("Downloading from URL: %s", link)
+                if not os.path.exists(
+                        os.path.join(self.__config['XAMVN']['default-download-dir'], str(self.__current_page))):
+                    os.makedirs(
+                        os.path.join(self.__config['XAMVN']['default-download-dir'], str(self.__current_page)))
+                file_type = str(req.headers['Content-Type']).split('/')[-1]
+                with open(os.path.join(self.__config['XAMVN']['default-download-dir'], str(self.__current_page),
+                                       str(count) + "." + file_type), 'wb') as file:
+                    file.write(req.content)
+                self.__log.info("Done getting URL: %s" % link)
+
+    def crawl(self,):
+        # Get URL, to get some cookies, blah blah, and parse them to the fucking request
+        self.__current_page = 1
+        if self.__from_page is not None:
+            self.__current_page = int(self.__from_page)
+        self.__log.info("Setting cookies")
+        self.__log.info(self.__cookies)
+        for cook in self.__cookies:
+            self.__log.debug('Adding cookie: %s -- value: %s' %
+                             (cook, self.__cookies[cook]))
+            self.f.cookies.set(cook, self.__cookies[cook])
+            # break
+        while self.__current_page < self.__to_page + 1:
+            self.__log.info('Getting page: %s' % str(self.__current_page))
+            req = self.f.get(self.__URL + '/page-' +
+                             str(self.__current_page), headers=self.__requests_headers, cookies=self.f.cookies)
+
+            soup = BeautifulSoup(req.text, "html.parser")
+            images = soup.find_all('img')
+
+            # Set headers to request headers
+            # Get all the images
+            if len(images) != 0:
+                self.get_element_url_and_download(images)
+            else:
+                self.__log.info('Cannot find any images')
+            # Get all the videos
+            videos = soup.find_all('source')
+            if len(videos) != 0:
+                self.get_element_url_and_download(videos)
+            self.__current_page += 1
